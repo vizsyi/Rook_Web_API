5c9e2dcd594c21dff12732c792e2062b52d2dcfa
Vizsy István <steve.visy@gmail.com>
2023-12-02 19:28:03 +0100

    #2: Revision of the Identity tables

--- Rook01/Data/EF/ApplicationDBContext.cs
+++ Rook01/Data/EF/ApplicationDBContext.cs
@@ -1,18 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Rook01.Data.Identity;
+using Rook01.Models.Auth;
+using Rook01.Models.Auth.Tokens;
 using Rook01.Models.Celeb;
 using System.Reflection.Metadata;
 
 namespace Rook01.Data.EF
 {
-    public class ApplicationDBContext : IdentityDbContext<ApplicationUser, IdentityRole, String>
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
     {
         public ApplicationDBContext(DbContextOptions options) : base(options)
         {
 
         }
+
+        public DbSet<RefreshToken> RefreshToken { get; set; }
 
         public DbSet<Profession> Profession { get; set; }
 
@@ -23,6 +26,40 @@ namespace Rook01.Data.EF
             base.OnModelCreating(modelBuilder);
             modelBuilder.HasDefaultSchema("Auth");
 
+            //ApplicationUser
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.UserKey)
+                .HasColumnType("char");
+
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.UserName).HasMaxLength(64);
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.NormalizedUserName).HasMaxLength(64);
+
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.Email).HasMaxLength(64);
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.NormalizedEmail).HasMaxLength(64);
+
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.PasswordHash)
+                .HasColumnType("char").HasMaxLength(94);
+
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.SecurityStamp)
+                .HasColumnType("char").HasMaxLength(42);
+
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.ConcurrencyStamp)
+                .HasColumnType("char").HasMaxLength(36);
+
+            modelBuilder.Entity<ApplicationUser>().Property(x => x.PhoneNumber)
+                .HasColumnType("char").HasMaxLength(1);
+
+            //IdentityRole
+            modelBuilder.Entity<IdentityRole>().Property(x => x.Name).HasMaxLength(32);
+            modelBuilder.Entity<IdentityRole>().Property(x => x.NormalizedName).HasMaxLength(32);
+
+            //RefreshToken
+            modelBuilder.Entity<RefreshToken>().Property(x => x.UserKey).HasColumnType("char");
+
+            modelBuilder.Entity<RefreshToken>().Property(x => x.SecKey).HasColumnType("char");
+
+            modelBuilder.Entity<RefreshToken>().Property(x => x.LongToken).HasColumnType("char");
+
+            //Celeb
             modelBuilder.Entity<Profession>()
                 .ToTable("Profession", schema: "Celeb");
 
---
+++ Rook01/Models/Auth/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Rook01.Models.Auth.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rook01.Models.Auth
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public ApplicationUser() : base()
+        {
+        }
+        public ApplicationUser(RegisterDTO model) : base(model.UserName)
+        {
+            //this.Id = Guid.NewGuid().ToString();
+            //this.UserName= model.UserName;
+            this.Email= model.Email;
+            //this.SecurityStamp = Guid.NewGuid().ToString(); //todo: test
+        }
+
+        [Required]
+        [StringLength(8)]
+        public string UserKey { get; set; }
+
+        //[Required]
+        //public string NormalizedPetname { get; set; } = "";
+    }
+}
